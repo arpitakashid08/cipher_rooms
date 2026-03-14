cc40fd3d5a4fe00d7b60f4ac33a17f08a8507985
Arpita Kashid <arpitakashid@gmail.com>
2026-03-14 22:48:57 +0530

    Consolidate API into single serverless function

--- api/auth/logout.js
+++
@@ -1,8 +0,0 @@
-import { ensureSchema, clearSessionCookie } from "../_db.js";
-
-export default async function handler(req, res) {
-  if (req.method !== "POST") return res.status(405).json({ error: "Method not allowed" });
-  await ensureSchema();
-  clearSessionCookie(res);
-  return res.json({ ok: true });
-}
